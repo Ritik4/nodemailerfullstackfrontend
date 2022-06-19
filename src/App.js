c753aeef089857f378a68dc64729cb4959e94b4f
Ritik4 <68722209+Ritik4@users.noreply.github.com>
2022-06-19 15:24:10 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "@mui/material/Button";
+import { Box } from "@mui/system";
+import TextField from "@mui/material/TextField";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+import { useState } from "react";
+import styles from "./style";
+import format from "date-fns/format";
+import axios from "axios";
 
 function App() {
+  const [value, setValue] = useState(new Date());
+  const [Schedule, setSchedule] = useState({
+    emailFrom: "",
+    emailTo: "",
+    time: "",
+    month: "",
+    day: "",
+  });
+  const onSubmit = (data) => {
+    axios.get("http://localhost:8000", {
+      params: data,
+    });
+
+    setSchedule({
+      emailFrom: "",
+      emailTo: "",
+      time: "",
+      month: "",
+      day: "",
+    });
+  };
+  const onStop = (data) => {
+    axios.get("http://localhost:8000/stop");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box sx={styles.App}>
+      <Box component="div" sx={styles.container}>
+        <TextField
+          id="outlined-basic"
+          label="Email from"
+          value={Schedule.emailFrom}
+          variant="outlined"
+          sx={{ marginBottom: "20px" }}
+          onChange={(e) => {
+            setSchedule({ ...Schedule, emailFrom: e.target.value });
+            console.log(e.target.value);
+          }}
+        />
+        <TextField
+          id="outlined-basic"
+          value={Schedule.emailTo}
+          label="Email to"
+          variant="outlined"
+          sx={{ marginBottom: "20px" }}
+          onChange={(e) => {
+            setSchedule({ ...Schedule, emailTo: e.target.value });
+            console.log(e.target.value);
+          }}
+        />
+
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <TimePicker
+            ampm={false}
+            openTo="hours"
+            views={["hours", "minutes", "seconds"]}
+            inputFormat="HH:mm:ss"
+            mask="__:__:__"
+            label="Time"
+            value={value}
+            onChange={(newValue) => {
+              setValue(newValue);
+              setSchedule({ ...Schedule, time: format(newValue, "ss mm HH") });
+              // console.log(format(newValue, "ss mm HH"));
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
+        <TextField
+          id="outlined-basic"
+          label="Month"
+          value={Schedule.month}
+          variant="outlined"
+          sx={{ margin: "20px 0px" }}
+          onChange={(e) => {
+            setSchedule({ ...Schedule, month: e.target.value });
+            console.log(e.target.value);
+          }}
+        />
+        <TextField
+          id="outlined-basic"
+          label="week Day"
+          value={Schedule.day}
+          variant="outlined"
+          sx={{ marginBottom: "20px" }}
+          onChange={(e) => {
+            setSchedule({ ...Schedule, day: e.target.value });
+            console.log(e.target.value);
+          }}
+        />
+        <Button
+          sx={{ marginTop: "20px" }}
+          variant="contained"
+          onClick={() => onSubmit(Schedule)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Send
+        </Button>
+        <Button
+          sx={{ marginTop: "20px" }}
+          variant="contained"
+          onClick={() => onStop()}
+        >
+          Stop
+        </Button>
+      </Box>
+    </Box>
   );
 }
 
---
+++ src/style.js
@@ -0,0 +1,18 @@
+const styles = {
+  App: {
+    display: "flex",
+    width: "1400px",
+    margin: "auto",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    height: "100vh",
+    padding: "0px 40px",
+  },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    width: "300px",
+  },
+};
+
+export default styles;
